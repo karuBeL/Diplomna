5c8e3ed8f8b9ca21551028fdf6aafb690e69a04f
karuBeL <burak.bali420@gmail.com>
2023-12-03 00:32:11 +0200

    Minor fixes to AnimationTree

--- Entities/Enemy/EnemyStates/EnemyIdle.gd
+++ Entities/Enemy/EnemyStates/EnemyIdle.gd
@@ -1,16 +1,17 @@
 extends Enemy
 
-var range : Area3D 
+@onready var range : Area3D = $"../../PursueRange"
 
 func inRange(body):
+	if body != player:
+		return
 	print("inRange")
-	$"../../AnimationPlayer".play("")
 	state_machine.transition_to("Pursue")
 
 func enter(msg := {}) -> void:
-	$"../../AnimationPlayer".play("idle")
-	range = $"../../PursueRange" as Area3D
-	range.body_entered.connect(inRange)
+	$"../../AnimationTree".set("parameters/Idle_Pursue/blend_amount", 0)
+	if !range.body_entered.is_connected(inRange):
+		range.body_entered.connect(inRange)
 	#range.connect("area_entered", inRange)
 
 func update(delta: float) -> void:
--- Entities/Enemy/EnemyStates/Pursue.gd
+++ Entities/Enemy/EnemyStates/Pursue.gd
@@ -7,11 +7,11 @@ func enter(msg := {}) -> void:
 func physics_update(delta: float) -> void:
 	var target_position = (player.position - enemy.position).normalized()
 	if enemy.position.distance_to(player.position) > 2:
-		$"../../AnimationPlayer".play("pursue")
+		$"../../AnimationTree".set("parameters/Idle_Pursue/blend_amount", 1)
 		enemy.velocity.x = target_position.x * enemy.speed
 		enemy.velocity.z = target_position.z * enemy.speed
 	else:
 		enemy.velocity.x = move_toward(enemy.velocity.x, 0, enemy.speed)
 		enemy.velocity.z = move_toward(enemy.velocity.z, 0, enemy.speed)
-		$"../../AnimationPlayer".play("idle")
+		$"../../AnimationTree".set("parameters/Idle_Pursue/blend_amount", 0)
 	enemy.move_and_slide()
