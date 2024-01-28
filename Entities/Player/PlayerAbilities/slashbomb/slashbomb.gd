a674cb5f233fd8cb6a73f1a6956c9608030c058e
karuBeL <burak.bali420@gmail.com>
2024-01-28 23:21:49 +0200

    Added slashbomb ability

--- Entities/Player/PlayerAbilities/slashbomb/slashbomb.gd
+++ Entities/Player/PlayerAbilities/slashbomb/slashbomb.gd
@@ -5,16 +5,27 @@ var weapon : Node3D
 var inAnimation : bool
 var playerStateMachine : StateMachine
 var timer : SceneTreeTimer
-
-
+var bomb_collision : Area3D
+var sword_model : Node3D
+var bodies 
+var anim_player : AnimationPlayer
 
 func _ready():
 	#timer.wait_time = 1
+	bomb_collision = $Area3D
+	anim_player = $AnimationPlayer
 	timer = get_tree().create_timer(0.0)
 	var player = get_tree().get_nodes_in_group("player")[0]
 	collision = player.get_node("WeaponCollision")
 	weapon = player.get_node("Weapon")
 	playerStateMachine = player.get_node("StateMachine")
+	sword_model = player.get_node("Weapon/swordidk")
+
+	
+func explode():
+	var damage = 20 + bodies.size() * 5
+	for body in bodies:
+		body.apply_damage(damage)
 
 func execute():
 	if timer.time_left != 0:
@@ -24,6 +35,9 @@ func execute():
 	if inAnimation:
 		return
 	inAnimation = true
+	bodies = bomb_collision.get_overlapping_bodies()
+	global_position = sword_model.global_position
+	anim_player.play("explode")
 	collision.get_collisions()
 	tween1(weapon)
 
--- Entities/Player/States/executeAbility.gd
+++ Entities/Player/States/executeAbility.gd
@@ -2,8 +2,8 @@ extends PlayerState
 
 var index = 0
 func enter(_msg := {}) -> void:
-	player.load_ability("knockbackdash")
-	player.load_ability("slash")#Testing abilities
+	player.load_ability("dash")
+	player.load_ability("slashbomb")#Testing abilities
 	index = _msg.get("index")
 	player.abilities[index].execute()
 	#state_machine.transition_to("Run")
